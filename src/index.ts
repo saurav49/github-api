050ca7973f074f75d45b0ad0b3a34a3a26851458
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2025-03-09 13:07:20 +0530

    feat: created health and get github data by username endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { userByNameUrl } from "./consts/urls";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const username = process?.env.USERNAME || "";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is up and running",
+  });
+});
+
+app.get("/github", async (req: Request, res: Response) => {
+  try {
+    const response = await fetch(`${userByNameUrl}${username}`);
+    const data = await response.json();
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "unable to fetch data",
+    });
+  }
 });
 
 app.listen(port, () => {
