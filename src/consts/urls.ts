fed3e7a694a9a959da900083c24d8a77bbde4923
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2025-03-09 13:31:14 +0530

    feat: add cors, created get repo by reponame, update urls

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -1,2 +1,3 @@
-const userByNameUrl = `https://api.github.com/users/`;
-export { userByNameUrl };
+const userByNameUrl = `https://api.github.com/users`;
+const repoByNameUrl = `https://api.github.com/repos`;
+export { userByNameUrl, repoByNameUrl };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { userByNameUrl } from "./consts/urls";
+import { userByNameUrl, repoByNameUrl } from "./consts/urls";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const username = process?.env.USERNAME || "";
+const username = process?.env.GITHUB_USERNAME || "";
+
+app.use(helmet());
+app.use(cors());
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({
@@ -17,7 +24,7 @@ app.get("/health", (req: Request, res: Response) => {
 
 app.get("/github", async (req: Request, res: Response) => {
   try {
-    const response = await fetch(`${userByNameUrl}${username}`);
+    const response = await fetch(`${userByNameUrl}/${username}`);
     const data = await response.json();
     res.status(200).json({
       success: true,
@@ -28,6 +35,30 @@ app.get("/github", async (req: Request, res: Response) => {
     res.status(500).json({
       success: false,
       message: "unable to fetch data",
+    });
+  }
+});
+
+app.get("/github/:repo_name", async (req: Request, res: Response) => {
+  const { repo_name } = req.params;
+  const repoUrl = `${repoByNameUrl}/${username}/${repo_name}`;
+  try {
+    const response = await fetch(repoUrl);
+    if (!response.ok) {
+      res.status(404).json({
+        success: false,
+        message: "Repository not found",
+      });
+    }
+    const data = await response.json();
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to fetch data from GitHub",
     });
   }
 });
